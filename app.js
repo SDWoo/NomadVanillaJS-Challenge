8e3c546bb8600fc4b6d837108a2c6a9eac3f5cda
sdwoo <damjang67@naver.com>
2022-07-07 20:33:34 +0900

    feat: 3th-day

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-alert("hi");
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  title.style.color = "blue";
+}
+function handleMouseEnter() {
+  title.style.color = "red";
+  title.innerText = "Mouse is here";
+}
+function handleMouseLeave() {
+  title.style.color = "green";
+  title.innerText = "Mouse is gone";
+}
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copy!!");
+}
+function handleWindowOffline() {
+  alert("SoS not in wifi!");
+}
+function handleWindowOnline() {
+  alert("ALL GOOOD!");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
