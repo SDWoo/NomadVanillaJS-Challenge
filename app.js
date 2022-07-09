2539c008f4523326fe4b0b50b68db9739ccc416b
sdwoo <damjang67@naver.com>
2022-07-09 19:03:16 +0900

    test: classList.toggle

--- app.js
+++ app.js
@@ -1,35 +1,6 @@
-const title = document.querySelector("div.hello:first-child h1");
-
+const h1 = document.querySelector("div.hello:first-child h1");
 function handleTitleClick() {
-  title.style.color = "blue";
-}
-function handleMouseEnter() {
-  title.style.color = "red";
-  title.innerText = "Mouse is here";
-}
-function handleMouseLeave() {
-  title.style.color = "green";
-  title.innerText = "Mouse is gone";
-}
-function handleWindowResize() {
-  document.body.style.backgroundColor = "tomato";
+  h1.classList.toggle("clicked");
 }
 
-function handleWindowCopy() {
-  alert("copy!!");
-}
-function handleWindowOffline() {
-  alert("SoS not in wifi!");
-}
-function handleWindowOnline() {
-  alert("ALL GOOOD!");
-}
-
-title.addEventListener("click", handleTitleClick);
-title.addEventListener("mouseenter", handleMouseEnter);
-title.addEventListener("mouseleave", handleMouseLeave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click", handleTitleClick);
