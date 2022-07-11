523b6b23c190756b78037e54d0ddd45488890f12
sdwoo <damjang67@naver.com>
2022-07-11 14:27:17 +0900

    feat: day8-login

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-const h1 = document.querySelector("div.hello:first-child h1");
-function handleTitleClick() {
-  h1.classList.toggle("clicked");
-}
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const greeting = document.querySelector("#greeting");
 
-h1.addEventListener("click", handleTitleClick);
+const HIDDEN_CLASS_NAME = "hidden";
+const USER_NAME_KEY = "username";
+const savedUserName = localStorage.getItem("username");
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const userName = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASS_NAME);
+  localStorage.setItem(USER_NAME_KEY, userName);
+  paintGreeting(userName);
+}
+function paintGreeting(username) {
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASS_NAME);
+}
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASS_NAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  paintGreeting(savedUserName);
+}
